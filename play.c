529fc120ff49be82da5e21eb3da699574c5d85c9
Persnickity <wdb32@drexel.edu>
2014-03-17 00:49:16 -0400

    play.c, Giving meaningful data oh yeah

--- play.c
+++ play.c
@@ -8,24 +8,31 @@
 #include <SDL.h>
 #include <SDL_mixer.h>
 
-#define BUFFER 1024
+#define MUSBUFFER 1024
 
-static int audio_open = 0;
-static Mix_Music *music = NULL;
-Sint16 stream[2][BUFFER*2*2];
-int len = BUFFER*2*2;
-Sint16 *buf;
+Sint16 stream[2][MUSBUFFER*2*2];
+int len=MUSBUFFER*2*2, which=0;
 
 void postmix(void *udata, Uint8 *_stream, int _len)
 {
-        memcpy(buf,_stream,len);
+	len=_len;
+        memcpy(stream[(which+1)%2],_stream,len);
+	which = (which + 1) % 2;
 }
-int barheight() 
-{	
+/*
+	Sint16 stream[2][MUSBUFFER*2*2];
+	#define MUSBUFFLEN = MUSBUFFER*2*2
+	int which=0;
+*/
+
+int getheight() 
+{
+	Sint16 *buf;
+	buf = stream[which];
 	int height = 0;
 	int x;//position
 	int c;//count
-	for(x=0;x<len;x++)
+	for(x=0;x<MUSBUFFER;x++)
 	{
 		if (buf[x] > 0)
 		{
@@ -37,7 +44,7 @@ int barheight()
 	return height;	
 }
 
-void CleanUp(int exitcode)
+/*void CleanUp(int exitcode)
 {
         if( Mix_PlayingMusic() ) {
                 Mix_FadeOutMusic(1500);
@@ -47,69 +54,120 @@ void CleanUp(int exitcode)
                 Mix_FreeMusic(music);
                 music = NULL;
         }
-        if ( audio_open ) {
-                Mix_CloseAudio();
-                audio_open = 0;
-        }
+        Mix_CloseAudio();
         SDL_Quit();
         exit(exitcode);
+}
+*/
+
+void startMusic(char* song)
+{
+	printf("startMusic\n");
+	Mix_Music *music = NULL;
+
+	// aud = audio 
+	int aud_rate;
+	Uint16 aud_format;
+	int aud_channels;
+	int aud_volume = MIX_MAX_VOLUME;
+
+	// Initialize 
+	// Hard coded to stereo settings 
+	aud_rate = 22050;
+	aud_format = AUDIO_S16;
+	aud_channels = 2;
+
+	// Initialize SDL library 
+	if ( SDL_Init(SDL_INIT_AUDIO < 0 )) 
+	{
+		fprintf(stderr, "Couldn't initialize the SDL, EC: %s\n",SDL_GetError());
+	//	return(255);
+	}
+
+	// Initialize audio device 
+	if (Mix_OpenAudio(aud_rate, aud_format, aud_channels, MUSBUFFER) < 0)
+	{
+		fprintf(stderr, "Couldn't open audio, EC: %s\n", SDL_GetError());
+	//	return (2);
+	}
+	// set up the post mix processor 
+	Mix_SetPostMix(postmix,song);
+
+	// Set volume for music 
+	Mix_VolumeMusic(aud_volume);
+
+	// Load music 
+	music = Mix_LoadMUS(song);
+
+	// Play and exit 
+	Mix_FadeInMusic(music, -1, 1000);
+	int tmp;
+	while ( Mix_PlayingMusic() || Mix_PausedMusic() ) 
+	{
+		tmp = getheight();
+		SDL_Delay(100);
+	}
+
+//	CleanUp(0);
+
 }
 
 int main(int argc, char *argv[])
 {
-	/* aud = audio */
+	startMusic(argv[1]);
+	fflush(stdout);
+	fflush(stdout);
+	int tmp = getheight();
+	return 0;
+}
+/*
+
+int main(int argc, char *argv[])
+{
+	Mix_Music *music = NULL;
 	int aud_rate;
 	Uint16 aud_format;
 	int aud_channels;
 	int aud_buffers;
 	int aud_volume = MIX_MAX_VOLUME;
 
-	/* Initialize */
-	/* Hard coded to stereo settings */
 	aud_rate = 22050;
 	aud_format = AUDIO_S16;
 	aud_channels = 2;
 
-	/* Initialize SDL library */
 	if ( SDL_Init(SDL_INIT_AUDIO < 0 )) 
 	{
 		fprintf(stderr, "Couldn't initialize the SDL, EC: %s\n",SDL_GetError());
 		return(255);
 	}
 
-	/* Initialize audio device */
-	if (Mix_OpenAudio(aud_rate, aud_format, aud_channels, BUFFER) < 0)
+	if (Mix_OpenAudio(aud_rate, aud_format, aud_channels, MUSBUFFER) < 0)
 	{
 		fprintf(stderr, "Couldn't open audio, EC: %s\n", SDL_GetError());
 		return (2);
-	} else {
-		Mix_QuerySpec(&aud_rate, &aud_format, &aud_channels);
-		printf("Opened audio successfully\n");
 	}
-	audio_open = 1;
-	/* set up the post mix processor */
 	Mix_SetPostMix(postmix,argv[1]);
 
-	/* Set volume for music */
 	Mix_VolumeMusic(aud_volume);
 
-	/* Load music */
 	music = Mix_LoadMUS(argv[1]);
 
-	while (music)
+	Mix_FadeInMusic(music, -1, 1000);
+	int tmp;
+	printf("got out of Mix_PlayMusic\n");
+
+	while ( Mix_PlayingMusic() || Mix_PausedMusic()) 
 	{
-		/* Play and exit */
-		Mix_PlayMusic(music, -1);
-		while ( Mix_PlayingMusic() || Mix_PausedMusic()) 
-		{
-			SDL_Delay(100);
-		}
-	
-		Mix_FreeMusic(music);
-		music = NULL;
-		SDL_Delay(500);
+		SDL_Delay(100);
+		fflush(stdout);
+		fflush(stdout);
+		tmp = getheight();
+		SDL_Delay(100);
+		printf("Height : %d\n",tmp);
 	}
-	CleanUp(0);
+
+	//CleanUp(0);
 	
 	return 0;
 }
+*/
